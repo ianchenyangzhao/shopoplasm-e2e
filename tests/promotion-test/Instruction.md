2da98cd542fbfa2a0a2a2d1b937522acbacc6b8d
chzhao <chenyang@gravitybrands.com>
2016-12-02 14:58:50 -0800

    add cookies for gwp test cases

--- tests/promotion-test/Instruction.md
+++ tests/promotion-test/Instruction.md
@@ -6,5 +6,28 @@
     - expect any purchase to have 5 dollar discount
 2. Percentage discount (fjcollect1):
     - expect any purchase to have 20% off
-3. Freeshipping discount (freeups):
+3. Free shipping discount (freeups):
     - expect any purchase to have free shipping
+4. Gift with purchase:
+    - expect extra ring being added to cart.
+5. Add discount first, then activate a gift with purchase:
+    - apply discount to cart
+    - expect gwp activated and ask to replace discount
+    - confirm:
+        - expect discount being removed
+        - expect gift with purchase in cart
+    - reject:
+        - expect discount in cart
+        - expect gift with purchase disappear
+
+6. Activate gift with purchase first, then apply discount code:
+    - apply gift with purchase added to cart
+    - apply discount expect alert to replace gift with purchase
+    - confirm: 
+        - expect discount being added to cart
+        - expect gift with purchase being removed
+   - reject:         - expect gift with purchase in cart
+        - expect discount disappear
+
+
+
--- tests/promotion-test/regular-promotion-test.js
+++ tests/promotion-test/regular-promotion-test.js
@@ -18,6 +18,13 @@ const purchaseSetup = function(client) {
         .waitForElementPresent("#addToCart")
         .click("#addToCart")
         .waitForElementPresent("form#the-cart-form input#agree")
+
+
+    return client
+}
+
+const gotoCheckout = function(client) {
+    client
         .perform(function() {
             console.log("Going to checkout page");
         })
@@ -54,10 +61,11 @@ const purchaseSetup = function(client) {
 }
 
 module.exports = {
-    'Discount Code Test: fjcollect1: 5 dollar off': function(client) {
+    "Discount Code Test: fjcollect1: 5 dollar off": function(client) {
         clientWithCart = purchaseSetup(client);
         var discountCode = 'fjcollect1';
 
+        clientWithCart = gotoCheckout(clientWithCart);
         clientWithCart
             .waitForElementPresent("a.code-link.animate-link")
             .click("a.code-link.animate-link")
@@ -68,13 +76,14 @@ module.exports = {
             .click('.discount-list .btn-block')
             .pause(5000)
             .saveScreenshot('./reports/dollar_amount.png')
-        
+
         clientWithCart.end();
     },
-    'Discount Code Test: fjcollect: 20% off': function(client) {
+    "Discount Code Test: fjcollect: 20% off": function(client) {
         clientWithCart = purchaseSetup(client);
         var discountCode = 'fjcollect';
 
+        clientWithCart = gotoCheckout(clientWithCart);
         clientWithCart
             .waitForElementPresent("a.code-link.animate-link")
             .click("a.code-link.animate-link")
@@ -88,10 +97,11 @@ module.exports = {
 
         clientWithCart.end();
     },
-    'Discount Code Test: freeups: Freeshipping': function(client) {
+    "Discount Code Test: freeups: Freeshipping": function(client) {
         clientWithCart = purchaseSetup(client);
         var discountCode = 'freeups';
-        
+
+        clientWithCart = gotoCheckout(clientWithCart);
         clientWithCart
             .waitForElementPresent("a.code-link.animate-link")
             .click("a.code-link.animate-link")
@@ -105,4 +115,58 @@ module.exports = {
 
         clientWithCart.end();
     },
+    "Gitf with purchase: gwp-test: Lemon Drop - Citrus Verbena Jewel Candle": function(client) {
+        // set promo with cookie
+        client
+            .url("https://test-fragrant-jewels-store.myshopify.com")
+            .setCookie({
+                name: "test_cookie",
+                value: "test_value",
+                path: "/",
+                domain: "https://test-fragrant-jewels-store.myshopify.com",
+            })
+            .url("https://test-fragrant-jewels-store.myshopify.com")
+            .getCookie('test_cookie', function callback(result) {
+                console.log(result);
+                
+                this.assert.equals(result.name, 'test_cookie');
+                this.assert.equal(result.value, 'test_value');
+            })
+            .pause(5000);
+        var gwpCode = 'gwp-test';
+
+        clientWithCart = purchaseSetup(client);
+        clientWithCart
+            .keys(['\uE00C'])
+            .pause(10000)
+            .perform(function() {
+                console.log('prepare to take screen shots of current cart, expect gwp show up')
+            })
+            //     .saveScreenshot('./reports/gwpCart.png')
+            //     .click("form#the-cart-form input#agree")
+            //     .click("#checkout")
+            //     .waitForElementPresent("input[name='email']")
+            //     .setValue("input[name='email']", "fjtest@fake.com")
+            //     .waitForElementPresent("input[name='first_name']")
+            //     .setValue("input[name='first_name']", "fakejohn")
+            //     .waitForElementPresent("input[name='last_name']")
+            //     .setValue("input[name='last_name']", "doe")
+            //     .waitForElementPresent("input[name='address1']")
+            //     .setValue("input[name='address1']", "e w4 st")
+            //     .waitForElementPresent("input[name='city']")
+            //     .setValue("input[name='city']", "los angeles")
+            //     .setValue("select[name='state']", "CA")
+            //     .waitForElementPresent("form input[name='zipcode']")
+            //     .click("form input[name='zipcode']")
+            //     .waitForElementPresent("input[name='zipcode']")
+            //     .setValue("input[name='zipcode']", "90058")
+            //     .waitForElementPresent("form input[name='phone']")
+            //     .click("form input[name='phone']")
+            //     .waitForElementPresent("input[name='phone']")
+            //     .setValue("input[name='phone']", "8882227782")
+            //     .pause(5000)
+            //     .saveScreenshot('./reports/gwpInCart.png')
+
+        // clientWithCart.end();
+    }
 };
